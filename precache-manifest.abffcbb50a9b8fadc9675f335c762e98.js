db166962e0d9ebff489a4bc3c00448a388a29164
wm2ch <d1479117@urhen.com>
2019-11-22 14:29:56 +0300

    Updates

---
+++ precache-manifest.abffcbb50a9b8fadc9675f335c762e98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2df34b0e21e33c9983613fab4186bd",
+    "url": "/index/index.html"
+  },
+  {
+    "revision": "1a90dcfeee2d52fb25a0",
+    "url": "/index/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "72dc639c678ae4b5201a",
+    "url": "/index/static/js/2.1feb1e52.chunk.js"
+  },
+  {
+    "revision": "1a90dcfeee2d52fb25a0",
+    "url": "/index/static/js/main.fa4a5882.chunk.js"
+  },
+  {
+    "revision": "7c194c23acef5d0471b4",
+    "url": "/index/static/js/runtime-main.d62b6707.js"
+  }
+]);
